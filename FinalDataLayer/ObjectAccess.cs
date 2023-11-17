27f3978647c77de970a241ac03a3c23b8c586831
Mitchell Stirmel <MStirmel02@Gmail.com>
2023-11-17 08:54:50 -0600

    Adding objects functionality

---
+++ FinalDataLayer/ObjectAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalDataObjects;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace FinalDataLayer
+{
+    public class ObjectAccess
+    {
+        RestClient client = new RestClient();
+
+
+        public List<ObjectModel> GetObjectList()
+        {
+            List<ObjectModel> objects = new List<ObjectModel>();
+
+
+            RestRequest request = new RestRequest("https://localhost:44333/Object");
+
+            var response = client.Get(request);
+
+            try
+            {
+                objects = JsonConvert.DeserializeObject<List<ObjectModel>>(response.Content);
+                return objects;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
--- FinalFrontend/ObjectList.xaml.cs
+++ FinalFrontend/ObjectList.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FinalDataObjects;
+using FinalLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +22,52 @@ namespace FinalFrontend
     /// </summary>
     public partial class ObjectList : Window
     {
+        private UserModel _userModel = new UserModel();
+        private UserManager _userManager;
+        private ObjectManager _objectManager;
+
         public ObjectList()
         {
             InitializeComponent();
+        }
+        public ObjectList(UserModel userModel)
+        {
+            InitializeComponent();
+            _userModel = userModel;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _userManager = new UserManager();
+            if (_userModel.Equals(new UserModel()))
+            {
+                ChangeMenuForLogin();
+            }
+            else
+            {
+                ChangeMenuForLogOut();
+            }
+        }
+
+        private void ChangeMenuForLogin()
+        {
+            mnuAccount.Visibility = Visibility.Collapsed;
+            mnuLogout.Visibility = Visibility.Visible;
+            mnuCreateAccount.Visibility = Visibility.Collapsed;
+            mnuLogout.Header = _userModel.UserId;
+
+        }
+
+        private void ChangeMenuForLogOut()
+        {
+            _userModel = new UserModel();
+            mnuAccount.Visibility = Visibility.Visible;
+            mnuCreateAccount.Visibility = Visibility.Visible;
+            mnuLogout.Visibility = Visibility.Collapsed;
+        }
+
+        private void GetObjects()
+        {
+
         }
     }
 }
